a02d3d6600e17047a03f048692e24a5872829c75
Mahdi <hassanim430@gmail.com>
2023-07-17 21:35:35 +0430

    initial commit

---
+++ Redux.only/index.js
@@ -0,0 +1,42 @@
+// Steps
+
+// Initial states
+
+const initialState = {
+  count: 0,
+};
+
+// actions -- actions, actions creators
+
+// increment action creator
+const incrementAction = () => {
+  return {
+    type: "INCREMENT",
+  };
+};
+
+// decrement action creator
+const decrementAction = () => {
+  return {
+    type: "DECREMENT",
+  };
+};
+
+// reset action creator
+const resetAction = () => {
+  return {
+    type: "RESET",
+  };
+};
+
+// increment by amount action creator
+const increment_By_Amount_Action = () => {
+  return {
+    type: "INCREASE_BY_AMOUNT",
+  };
+};
+
+// reducers
+const counterReducer = (state = initialState, action) => {};
+
+// store
