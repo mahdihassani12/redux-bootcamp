fef9a22221d88099b898ccd9d1a30fe30020c3fa
Mahdi <hassanim430@gmail.com>
2023-07-18 19:13:57 +0430

    completing add post reducer and dispatch

--- Redux.Only.Post.App/index.js
+++ Redux.Only.Post.App/index.js
@@ -22,9 +22,44 @@ const removePostAction = (id) => {
 };
 
 // reducer
-const postReducer = (state = initialState, action) => {};
+const postReducer = (state = initialState, action) => {
+  if (action.type === "ADD_POST") {
+    return {
+      posts: [...state.posts, action.payload],
+    };
+  } else if (action.type === "REMOVE_POST") {
+    return {
+      posts: state.posts.filter((post) => {
+        return post.id !== action.payload;
+      }),
+    };
+  } else {
+    return state;
+  }
+};
 
 // store
 const store = createStore(postReducer);
+store.subscribe(() => {
+  const data = store.getState();
+  console.log(data);
+});
 
 // dispatch
+store.dispatch(
+  addPostAction({
+    id: 1,
+    title: "First post",
+  })
+);
+
+store.dispatch(
+  addPostAction({
+    id: 2,
+    title: "Second post",
+  })
+);
+
+store.dispatch(
+  removePostAction(1)
+);
