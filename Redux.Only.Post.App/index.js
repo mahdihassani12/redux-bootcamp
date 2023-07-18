3d162a97a25e83761cbd4c6ef87c3371e28260dd
Mahdi <hassanim430@gmail.com>
2023-07-18 19:24:35 +0430

    combine reducers

--- Redux.Only.Post.App/index.js
+++ Redux.Only.Post.App/index.js
@@ -1,33 +1,56 @@
-const { createStore } = require("redux");
+const { createStore, combineReducers } = require("redux");
 
 // Initial state
 const initialState = {
   posts: [],
 };
 
-// actions - action creators
+const initialUsersState = {
+  users: []
+}
 
+// constants
+const ADD_POST = "ADD_POST";
+const REMOVE_POST = "REMOVE_POST";
+const ADD_USER = "ADD_USER";
+const REMOVE_USER = "REMOVE_USER";
+
+// actions - action creators
 const addPostAction = (post) => {
   return {
-    type: "ADD_POST",
+    type: ADD_POST,
     payload: post,
+  };
+};
+
+const addUserAction = (user) => {
+  return {
+    type: ADD_USER,
+    payload: user,
   };
 };
 
 const removePostAction = (id) => {
   return {
-    type: "REMOVE_POST",
+    type: REMOVE_POST,
+    payload: id,
+  };
+};
+
+const removeUserAction = (id) => {
+  return {
+    type: REMOVE_USER,
     payload: id,
   };
 };
 
 // reducer
 const postReducer = (state = initialState, action) => {
-  if (action.type === "ADD_POST") {
+  if (action.type === ADD_POST) {
     return {
       posts: [...state.posts, action.payload],
     };
-  } else if (action.type === "REMOVE_POST") {
+  } else if (action.type === REMOVE_POST) {
     return {
       posts: state.posts.filter((post) => {
         return post.id !== action.payload;
@@ -38,11 +61,33 @@ const postReducer = (state = initialState, action) => {
   }
 };
 
+const userReducer = (state = initialUsersState, action) => {
+  if (action.type === ADD_USER) {
+    return {
+      users: [...state.users, action.payload],
+    };
+  } else if (action.type === REMOVE_USER) {
+    return {
+      users: state.users.filter((user) => {
+        return user.id !== action.payload;
+      }),
+    };
+  } else {
+    return state;
+  }
+};
+
 // store
-const store = createStore(postReducer);
+const rootReducer = combineReducers({
+  users: userReducer,
+  posts: postReducer
+});
+const store = createStore(rootReducer);
+
 store.subscribe(() => {
   const data = store.getState();
-  console.log(data);
+  console.log("Posts ", data.posts);
+  console.log("Users ", data.users);
 });
 
 // dispatch
@@ -57,6 +102,13 @@ store.dispatch(
   addPostAction({
     id: 2,
     title: "Second post",
+  })
+);
+
+store.dispatch(
+  addUserAction({
+    id: 1,
+    name: "Mahdi Hassani",
   })
 );
 
