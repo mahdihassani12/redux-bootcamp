360e4b4cedb085649f121244e796100913d5de88
Mahdi <hassanim430@gmail.com>
2023-07-26 19:02:25 +0430

    adding action creators

--- React.Redux.Only.Post.App/src/redux/actions/postsActions.js
+++ React.Redux.Only.Post.App/src/redux/actions/postsActions.js
@@ -1,13 +1,25 @@
 import {
   FETCH_POSTS_REQUEST,
+  FETCH_POST_REQUEST,
   FETCH_POSTS_SUCCESS,
+  FETCH_POST_SUCCESS,
   FETCH_POSTS_FAILURE,
+  FETCH_POST_FAILURE
 } from "./postActionTypes";
+import axios from "axios";
+const apiURL = "https://jsonplaceholder.typicode.com/posts";
+
 const fetchPostsRequest = () => {
   return {
     type: FETCH_POSTS_REQUEST,
   };
 };
+
+const fetchPostRequest = () => {
+    return {
+      type: FETCH_POST_REQUEST,
+    };
+  };
 
 const fetchPostsSuccess = (posts) => {
   return {
@@ -16,6 +28,13 @@ const fetchPostsSuccess = (posts) => {
   };
 };
 
+const fetchPostSuccess = (post) => {
+    return {
+      type: FETCH_POST_SUCCESS,
+      payload: post,
+    };
+  };
+
 const fetchPostsFailure = (error) => {
   return {
     type: FETCH_POSTS_FAILURE,
@@ -23,9 +42,33 @@ const fetchPostsFailure = (error) => {
   };
 };
 
+const fetchPostFailure = (error) => {
+    return {
+      type: FETCH_POST_FAILURE,
+      payload: error,
+    };
+  };
+
 const fetchPostsAction = () => {
   return async (dispatch) => {
+    dispatch(fetchPostsRequest());
     try {
-    } catch (error) {}
+      const res = await axios.get(apiURL);
+      dispatch(fetchPostsSuccess(res.data));
+    } catch (error) {
+      dispatch(fetchPostsFailure(error));
+    }
   };
 };
+
+const fetchPostAction = (id) => {
+    return async (dispatch) => {
+      dispatch(fetchPostRequest());
+      try {
+        const res = await axios.get(`apiURL/${id}`);
+        dispatch(fetchPostSuccess(res.data));
+      } catch (error) {
+        dispatch(fetchPostFailure(error));
+      }
+    };
+  };
