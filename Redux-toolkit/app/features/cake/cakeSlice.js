cdefad1f0998df799bc23f70c6c1d8a4c4ec1834
Mahdi Hassai <hassanim430@gmail.com>
2023-08-06 05:22:10 +0430

    creating cake slices

---
+++ Redux-toolkit/app/features/cake/cakeSlice.js
@@ -0,0 +1,21 @@
+const { createSlice } = require("@reduxjs/toolkit");
+
+const initialState = {
+  numOfCakes: 10,
+};
+
+const cakeSlice = createSlice({
+  name: "cake",
+  initialState,
+  reducers: {
+    ordered: (state, action) => {
+      state.numOfCakes--;
+    },
+    restocked: (state, action) => {
+      state.numOfCakes += action.payload;
+    },
+  },
+});
+
+module.exports = cakeSlice.reducer;
+module.exports.cakeActions = cakeSlice.actions;
